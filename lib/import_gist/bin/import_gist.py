06a78f66744c9912df7f0e5faef2ff64bdcaf496
cosacog <cosacog@gmail.com>
2018-07-24 15:30:32 +0900

    Update import_gist.py

--- lib/import_gist/bin/import_gist.py
+++ lib/import_gist/bin/import_gist.py
@@ -1,10 +1,12 @@
 # coding: utf-8
 def import_gist(url_gist):
-    # import custom functions from gist.github.com
-    # usage: mod_name = import_gist(url_gist)
-    # params:
-    #   url_gist: url of gist. be sure to append '/raw/' to the gist url to load script, not html
-    #       e.g. https://gist.githubusercontent.com/cosacog/67ac95feef8a2a1cd373d43a86fe2c9c/raw/
+    '''
+    import custom functions from gist.github.com
+    usage: mod_name = import_gist(url_gist)
+    params:
+      url_gist: url of gist. be sure to append '/raw/' to the gist url to load script, not html
+          e.g. https://gist.githubusercontent.com/cosacog/67ac95feef8a2a1cd373d43a86fe2c9c/raw/
+    '''
     import os,sys, urllib, tempfile
     fname_func = 'tmp_func.py' # temporary file name of .py
     tmp_dir = tempfile.mkdtemp()

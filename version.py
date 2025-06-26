a33d9a4e1a048f4fa7b5d880815fb63859ba5cfa
K Ogata <cosacog@gmail.com>
2025-06-26 23:15:10 +0900

    correct version.py

--- version.py
+++ version.py
@@ -1,7 +1,9 @@
 # -*- coding:utf-8 -*-
-
-__version__ = "0.0.3"
-__author__ = "Katsuya Ogata"
+VERSION = "0.0.3"
+__version__ = VERSION
+__title__ = "import_gist"
+__license__ = "MIT License"
+__author__ = "cosac141"
 __description__ = "gistからPythonモジュールをインポートするための関数"
-
+__url__ = "https://github.com/cosacog/import_gist"
 __release_date__ = "2025-06-26"
